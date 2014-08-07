8f0c658d5060e36c730b72978244562a2629e621
Yonah Forst <yonah.forst@gmail.com>
2014-08-07 13:47:04 +0200

    store directly as json

--- db/migrate/20140119220107_add_options_to_spree_line_items.rb
+++ db/migrate/20140119220107_add_options_to_spree_line_items.rb
@@ -1,5 +1,5 @@
 class AddOptionsToSpreeLineItems < ActiveRecord::Migration
   def change
-    add_column :spree_line_items, :options, :string
+    add_column :spree_line_items, :options, :json
   end
 end
